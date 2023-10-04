6bd7cdb1e1bfc7aa64474f84c840a646aac92674
Priyanshu-Butterpaper1 <priyanshu.mishra@butterpaper.tech>
2023-10-04 19:24:23 +0530

    fixed code

--- Word Cloud.py
+++ Word Cloud.py
@@ -3,17 +3,48 @@ import numpy as np
 from PIL import Image
 import wikipedia
 from wordcloud import WordCloud, STOPWORDS
+from bs4 import BeautifulSoup
 
+try:
+    a = input("Enter the name of the topic for the word cloud: ")
+    search_results = wikipedia.search(a)
 
-a = str(input("Enter the name if which you want to make word cloud : "))
-title = wikipedia.search(a)[0]
-page = wikipedia.page(title)
-text = page.content
-print(text)
+    most_relevant_title = None
 
-bg = np.array(Image.open("black.jpg"))
-unwanted_words = set(STOPWORDS)
-wordclo = WordCloud(background_color="black", max_words=400,
-                    mask=bg, stopwords=unwanted_words)
-wordclo.generate(text)
-wordclo.to_file("sample.png")
+    for title in search_results:
+        try:
+            page = wikipedia.page(title)
+            if "disambiguation" not in page.summary.lower():
+                most_relevant_title = title
+                break
+        except wikipedia.exceptions.DisambiguationError:
+            continue
+
+    if most_relevant_title is None:
+        print("No relevant page found. Cannot create a word cloud.")
+        sys.exit()
+
+    page = wikipedia.page(most_relevant_title)
+    html = page.html()
+    lis = BeautifulSoup(html, features="html.parser").find_all('li')
+    text = " ".join([li.get_text() for li in lis])
+
+    bg = np.array(Image.open("black.jpg"))
+    unwanted_words = set(STOPWORDS)
+    wordcloud = WordCloud(
+        background_color="black",
+        max_words=400,
+        mask=bg,
+        stopwords=unwanted_words
+    )
+    wordcloud.generate(text)
+    wordcloud.to_file("sample2.png")
+    print("Word cloud created successfully as 'sample2.png'.")
+
+except wikipedia.exceptions.DisambiguationError:
+    print("Disambiguation error occurred. Cannot create a word cloud.")
+except wikipedia.exceptions.HTTPTimeoutError:
+    print("HTTP timeout error occurred. Please check your internet connection.")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
